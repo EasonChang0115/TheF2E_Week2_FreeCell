029bc8f12b64cc0b4d3988c6c65a5a9cffb9e12c
張仲豪 <goodjason@zhangzhonghaode-MacBook-Pro.local>
2019-07-18 00:30:38 +0800

    [header]

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div id="app">
-    <h1>fvz</h1>
+    <Header />
   </div>
 </template>
 <script>
+import Header from '@/components/Header.vue'
 export default {
-
+  components: {
+    Header
+  },
+  data() {
+    return {
+      pokeCards: [
+        {
+          id: 0,
+          number: 1,
+          color: 'pitch'
+        }
+      ]
+    }
+  }
 };
 </script>
 <style lang="scss">
-
+#app {
+  width: 100%;
+}
 </style>
