2bbfb0f1eb590719f771c9ec50dfb93f1b6dd98a
JasonChang <jason@ai-pattern.com>
2019-07-18 09:46:12 +0800

    字型

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
   <div id="app">
-    <Header />
   </div>
 </template>
 <script>
-import Header from '@/components/Header.vue'
 export default {
-  components: {
-    Header
-  },
   data() {
     return {
       pokeCards: [
