2892af4e24b06cd6d8279c0a1430be081d5c0a08
JasonChang <jason@ai-pattern.com>
2019-07-18 10:28:27 +0800

    loading page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div id="app">
+    <LoadingPage :showPage="showLoading"/>
   </div>
 </template>
 <script>
+import LoadingPage from '@/components/LoadingPage.vue';
+import { setTimeout } from 'timers';
 export default {
+  mounted() {
+    setTimeout(() => {
+      this.showLoading = false;
+    });
+  },
+  components: {
+    LoadingPage
+  },
   data() {
     return {
+      showLoading: true,
       pokeCards: [
         {
           id: 0,
@@ -13,12 +25,15 @@ export default {
           color: 'pitch'
         }
       ]
-    }
+    };
   }
 };
 </script>
 <style lang="scss">
 #app {
   width: 100%;
+  height: 100%;
+  background: rgb(82,82,82);
+  background: linear-gradient(180deg, rgba(82,82,82,1) 0%, rgba(0,31,29,1) 100%);
 }
 </style>
